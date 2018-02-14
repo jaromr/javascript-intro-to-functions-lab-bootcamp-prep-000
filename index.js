ecf71b19f86024ee4a83bf6fad8aefcb93b741f9
Jarom Roney <byutrueblue@gmail.com>
2018-02-14 21:00:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase()===string){
-    return "I can't hear you"
+    return "I can\'t hear you"
   }
   else if (string.toUpperCase()===string){
-    return "YES, INDEED!"
+    return "YES INDEED!"
   }
 }
